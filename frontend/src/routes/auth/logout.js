27baf987fed990eec4df8ef72bb7ed8cc376fa41
Nithin SenthurJeyamurugan <nithinkrsenthur@gmail.com>
2022-05-20 18:25:18 -0700

    update

--- frontend/src/routes/auth/logout.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { useAuth } from '../../services/users.service'
-import { Redirect } from 'react-router'
-
-export default function LogOut() {
-    const { logout } = useAuth()
-    logout()
-    return <Redirect to="/login" />
-}
