a40c93d8061952e5de14135af65acf0913c3e467
Nithin SenthurJeyamurugan <nithinkrsenthur@gmail.com>
2022-05-20 18:30:46 -0700

    client

---
+++ backend/client/src/routes/news/article.js
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from 'react'
+import { motion } from 'framer-motion'
+import { useParams, Link } from 'react-router-dom'
+import { Markup } from 'interweave'
+import { Alert, Loading } from '../../components/global'
+import { useArticles } from '../../services/articles.service'
+import { useAuth } from '../../services/users.service'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlusSquare, faTimesCircle, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faFacebookF, faLinkedinIn, faTwitter } from "@fortawesome/free-brands-svg-icons"
+import config from "../../config.json"
+
+export default function Article() {
+
+    const { retrieve } = useArticles()
+    const { isAdmin } = useAuth()
+    const { title } = useParams()
+    const [article, setArticle] = useState([])
+    const [error, setError] = useState()
+    const [isLoading, setLoading] = useState(true)
+
+    useEffect(() => {
+        retrieve({ title })
+            .then(res => {
+                if (res.articles[0]) {
+                    setArticle(res.articles[0])
+                } else {
+                    setError("Article not found")
+                }
+                setArticle(res.articles[0])
+                setLoading(false)
+            })
+    }, [])
+
+    return (
+        <div>
+            {isLoading
+                ?
+                <Loading />
+                : error
+                    ?
+                    <Alert message={error} />
+                    :
+                    <div className="article-wrapper">
+                        <motion.div
+                            initial={{ y: -100 }}
+                            animate={{ y: 0 }}
+                            transition={{ type: "spring", stiffness: 50 }}
+                        >
+                            <div className="banner">
+                                {article.img
+                                    &&
+                                    <div className="article-photo"
+                                        style={{
+                                            backgroundImage:
+                                                `linear-gradient(to bottom, 
+                                                                rgba(255,255,255,0) 0%, 
+                                                                rgba(0,0,0,0.5) 40%, 
+                                                                rgba(0,0,0,0.6) 100%),
+                                            url(${config.siteURL}/${article.img})`
+                                        }}
+                                    />
+                                }
+                                <div className="article-header">
+                                    <h1 className="article-title">{article.title}</h1>
+                                    <p className="article-info">
+                                        {new Date(article.date).toDateString()} | By <strong><Link to={`members/${article.name}`}>{article.name}</Link></strong>
+                                    </p>
+                                    <div className="article-social-media-buttons">
+                                        <ul>
+                                            <li><FontAwesomeIcon icon={faFacebookF} /></li>
+                                            <li><FontAwesomeIcon icon={faLinkedinIn} /></li>
+                                            <li><FontAwesomeIcon icon={faTwitter} /></li>
+                                            <li><FontAwesomeIcon icon={faEnvelope} /></li>
+                                        </ul>
+                                    </div>
+                                    <p className="article-description">{article.description}</p>
+                                </div>
+                            </div>
+                            <div className="main">
+                                {isAdmin() && <Link className="button" to={`/news/edit/${title}`}><FontAwesomeIcon icon={faPlusSquare} /> Edit Article</Link>}
+                                {isAdmin() && <Link className="button-red" to={`/news/delete/${title}`}><FontAwesomeIcon icon={faTimesCircle} /> Delete Article</Link>}
+                                <div className="article-body">
+                                        <Markup content={article.content} />
+                                </div>
+                            </div>
+                        </motion.div>
+                    </div>
+            }
+        </div>
+    )
+}
---
+++ backend/client/src/routes/news/index.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from 'react'
+import { Loading } from '../../components/global'
+import { Link } from 'react-router-dom'
+import { useAuth } from '../../services/users.service'
+import { useHistory } from 'react-router'
+import { useArticles } from '../../services/articles.service'
+import { faPlusSquare, faGlobe } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Panel from './panel'
+import '../../styles/articles.css'
+
+export default function NewsIndex({ title, page }) {
+
+    const { retrieve } = useArticles()
+    const { isAdmin } = useAuth()
+    const history = useHistory()
+    const [articles, setArticles] = useState([])
+    const [pageNumbers, setPageNumbers] = useState()
+    const [isLoading, setLoading] = useState(true)
+
+    // // adjust style of Material UI pagination component
+    // const useStyles = makeStyles((theme) => ({
+    //     root: {
+    //         "& > *": {
+    //             marginTop: theme.spacing(2),
+    //             justifyContent: "center",
+    //             display: 'flex'
+    //         }
+    //     }
+    // }))
+    // const classes = useStyles()
+
+    // Retrieve articles for current page
+    useEffect(() => {
+        retrieve({ title, page })
+            .then(res => {
+                setArticles(res.articles)
+                setPageNumbers(Math.ceil(res.total_results / 20))
+                setLoading(false)
+            })
+    }, [])
+
+    return (
+        <div className="main">
+            <div
+                className="primary"
+            >
+                <h1><FontAwesomeIcon icon={faGlobe} /> Latest News & Content</h1>
+            </div>
+            {isAdmin() && <Link className="button" to="/news/create"><FontAwesomeIcon icon={faPlusSquare} /> New Article</Link>}
+            {isLoading ?
+                <Loading />
+                :
+                <div className="articles-container">
+                    {
+                        articles.map((article, i) => {
+                            return <Panel key={article._id}
+                                title={article.title}
+                                date={article.date}
+                                description={article.description}
+                                img={article.img}
+                                author={article.name}
+                                delayTime={i}
+                            />
+                        })
+                    }
+                </div>
+            }
+            {/* <div className="pagination">
+                <Pagination
+                    className={classes.root}
+                    page={page}
+                    count={pageNumbers}
+                    onChange={(e, page) => {
+                        history.push(`news?page=${page}`)
+                        history.go(0)
+                    }}
+                    size="large"
+                />
+            </div> */}
+        </div>
+    )
+}
