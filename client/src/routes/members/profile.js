c51e8805d6a4729187011406711a99dacb04f82c
Nithin SenthurJeyamurugan <nithinkrsenthur@gmail.com>
2022-05-29 17:59:05 -0700

    added frontend

---
+++ client/src/routes/members/profile.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react'
+import { motion } from 'framer-motion'
+import { useParams } from 'react-router'
+import { useAuth } from '../../services/users.service'
+import { Alert, Loading, Avatar } from '../../components/global'
+import config from "../../config.json"
+
+export default function Profile() {
+
+    const { name } = useParams()
+    const { getUsers } = useAuth()
+    const [isLoading, setLoading] = useState(true)
+    const [member, setMember] = useState()
+    const [error, setError] = useState()
+
+    useEffect(() => {
+        getUsers(name)
+            .then(res => {
+                if (res.members[0]) {
+                    setMember(res.members[0])
+                } else {
+                    setError("User not found")
+                }
+                setLoading(false)
+            }
+            )
+    }, [])
+
+    return (
+        <div className="main">
+            {isLoading
+                ?
+                <Loading />
+                :
+                <motion.div
+                    initial={{ scale: 0.5 }}
+                    animate={{ scale: 1 }}
+                >
+                    {(error
+                        ?
+                        <Alert message={error} />
+                        :
+                        <div className="profile">
+                            <div className="card">
+                                <h1>{member.name}</h1>
+                                {member.avatar && <Avatar url={`${config.siteURL}/${member.avatar}`} />}
+                                <h2>{member.role} {member.specialty && <span>| {member.specialty}</span>}</h2>
+                                <strong>Joined:</strong> {new Date(member.date).toDateString()}
+                                <div>
+                                    <strong>Email:</strong> {member.email}<br />
+                                    <strong>Phone:</strong> {member.phone}<br />
+                                    {member.class && <span><strong>Class: </strong> {member.class}<br /></span>}
+                                    <strong>Location:</strong> {member.address.street && <span>{member.address.street}, </span>} {member.address.city}, {member.address.state}, {member.address.zipcode}< br />
+                                    {member.interests && <span><strong>Interests:</strong> {member.interests.join(', ')}<br /></span>}
+                                </div>
+                            </div>
+                            {member.description &&
+                                <div className="card">
+                                    <div>
+                                        <h1>About Me</h1>
+                                        {member.description}
+                                    </div>
+                                </div>
+                            }
+                        </div>
+                    )}
+                </motion.div>
+            }
+        </div>
+    )
+}
---
+++ client/src/routes/settings/user/user.settings.js
@@ -0,0 +1,257 @@
+import React, { useRef, useState } from 'react'
+import { motion } from 'framer-motion'
+import { Alert, Success } from '../../../components/global'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCog } from '@fortawesome/free-solid-svg-icons'
+import { useAuth } from '../../../services/users.service'
+import '../../../styles/settings.css'
+
+export default function UserSettings() {
+
+    const { edit, getID } = useAuth()
+
+    const description = useRef()
+    const password = useRef()
+    const passwordConfirmation = useRef()
+    const email = useRef()
+    const emailConfirmation = useRef()
+    const phone = useRef()
+    const state = useRef()
+    const city = useRef()
+    const street = useRef()
+    const zipcode = useRef()
+    
+    const [error, setError] = useState()
+    const [response, setResponse] = useState()
+
+    const changeAddress = async e => {
+        e.preventDefault()
+        console.log(`${city.current.value} and ${state.current.value} and ${zipcode.current.value} and ${street.current.value}`)
+        await edit(getID(), {
+            address: {
+                city: city.current.value,
+                state: state.current.value,
+                zipcode: zipcode.current.value,
+                ...(street.current.value && { street: street.current.value })
+            }
+        })
+            .then(res => {
+                if (res.error) setError(res.error)
+                else setResponse(res.status)
+            })
+    }
+
+    const changePassword = async e => {
+        e.preventDefault()
+        if (password.current.value !== passwordConfirmation.current.value) {
+            setError("Your passwords do not match.")
+        } else {
+            await edit(getID(), { password: password })
+                .then(res => {
+                    if (res.error) setError(res.error)
+                    else setResponse(res.status)
+                })
+        }
+    }
+
+    const changeDescription = async e => {
+        e.preventDefault()
+        await edit(getID(), { description: description.current.value })
+            .then(res => {
+                if (res.error) setError(res.error)
+                else setResponse(res.status)
+            })
+    }
+
+    const changeEmail = async e => {
+        e.preventDefault()
+        if (email.current.value !== emailConfirmation.current.value) {
+            setError("Your emails do not match.")
+        } else {
+            await edit(getID(), { email: email })
+                .then(res => {
+                    if (res.error) setError(res.error)
+                    else setResponse(res.status)
+                })
+        }
+    }
+
+    const changePhone = async e => {
+        e.preventDefault()
+        await edit(getID(), { phone: phone.current.value })
+            .then(res => {
+                if (res.error) setError(res.error)
+                else setResponse(res.status)
+            })
+    }
+
+    return (
+        <div className="main">
+            <motion.div
+                initial={{ scale: 0.5 }}
+                animate={{ scale: 1 }}
+                className="settings"
+            >
+                <h1><FontAwesomeIcon icon={faCog} /> Settings</h1>
+                {error && <Alert message={error} />}
+                {response && <Success message={response} />}
+                <div className="auth-form">
+                    <div>
+                        <h2>User Information</h2>
+                        <form onSubmit={changeEmail}>
+                            <strong>Email</strong>
+                            <div>
+                                <input type="email" ref={email} placeholder="Email..." required />
+                            </div>
+                            <div>
+                                <input type="email" ref={emailConfirmation} placeholder="Confirm Email..." required />
+                            </div>
+                            <motion.button
+                                className="form-button"
+                                whileHover={{ scale: 1.1 }}
+                                whileTap={{ scale: 0.9 }}
+                            >
+                                Update
+                            </motion.button>
+                        </form>
+                        <form onSubmit={changePassword}>
+                            <strong>Password</strong>
+                            <div>
+                                <input type="password" placeholder="Password..." ref={password} />
+                            </div>
+                            <div>
+                                <input type="password" placeholder="New Password..." ref={passwordConfirmation} />
+                            </div>
+                            <motion.button
+                                className="form-button"
+                                whileHover={{ scale: 1.1 }}
+                                whileTap={{ scale: 0.9 }}
+                            >
+                                Update
+                            </motion.button>
+                        </form>
+                        <h2>Personal Information</h2>
+                        <form onSubmit={changePhone}>
+                        <strong>Phone</strong>
+                            <div>
+                                <input type="text" ref={phone} placeholder="Phone..." required />
+                            </div>
+                            <motion.button
+                                className="form-button"
+                                whileHover={{ scale: 1.1 }}
+                                whileTap={{ scale: 0.9 }}
+                            >
+                                Update
+                            </motion.button>
+                        </form>
+                        <form onSubmit={changeAddress}>
+                            <strong>Address</strong>
+                            <div>
+                                <label for="street">Street </label>
+                                <input
+                                    type="text"
+                                    id="street"
+                                    ref={street}
+                                />
+                            </div>
+                            <div>
+                                <label for="city">City </label>
+                                <input
+                                    type="text"
+                                    id="city"
+                                    ref={city}
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <label for="state">State </label>
+                                <select id="state" ref={state}>
+                                    <option value="AL">Alabama</option>
+                                    <option value="AK">Alaska</option>
+                                    <option value="AZ">Arizona</option>
+                                    <option value="AR">Arkansas</option>
+                                    <option value="CA">California</option>
+                                    <option value="CO">Colorado</option>
+                                    <option value="CT">Connecticut</option>
+                                    <option value="DE">Delaware</option>
+                                    <option value="DC">District Of Columbia</option>
+                                    <option value="FL">Florida</option>
+                                    <option value="GA">Georgia</option>
+                                    <option value="HI">Hawaii</option>
+                                    <option value="ID">Idaho</option>
+                                    <option value="IL">Illinois</option>
+                                    <option value="IN">Indiana</option>
+                                    <option value="IA">Iowa</option>
+                                    <option value="KS">Kansas</option>
+                                    <option value="KY">Kentucky</option>
+                                    <option value="LA">Louisiana</option>
+                                    <option value="ME">Maine</option>
+                                    <option value="MD">Maryland</option>
+                                    <option value="MA">Massachusetts</option>
+                                    <option value="MI">Michigan</option>
+                                    <option value="MN">Minnesota</option>
+                                    <option value="MS">Mississippi</option>
+                                    <option value="MO">Missouri</option>
+                                    <option value="MT">Montana</option>
+                                    <option value="NE">Nebraska</option>
+                                    <option value="NV">Nevada</option>
+                                    <option value="NH">New Hampshire</option>
+                                    <option value="NJ">New Jersey</option>
+                                    <option value="NM">New Mexico</option>
+                                    <option value="NY">New York</option>
+                                    <option value="NC">North Carolina</option>
+                                    <option value="ND">North Dakota</option>
+                                    <option value="OH">Ohio</option>
+                                    <option value="OK">Oklahoma</option>
+                                    <option value="OR">Oregon</option>
+                                    <option value="PA">Pennsylvania</option>
+                                    <option value="RI">Rhode Island</option>
+                                    <option value="SC">South Carolina</option>
+                                    <option value="SD">South Dakota</option>
+                                    <option value="TN">Tennessee</option>
+                                    <option value="TX">Texas</option>
+                                    <option value="UT">Utah</option>
+                                    <option value="VT">Vermont</option>
+                                    <option value="VA">Virginia</option>
+                                    <option value="WA">Washington</option>
+                                    <option value="WV">West Virginia</option>
+                                    <option value="WI">Wisconsin</option>
+                                    <option value="WY">Wyoming</option>
+                                </select>
+                            </div>
+                            <div>
+                                <label for="zipcode">Zip Code </label>
+                                <input
+                                    type="number"
+                                    id="zipcode"
+                                    ref={zipcode}
+                                    required
+                                />
+                            </div>
+                            <motion.button
+                                className="form-button"
+                                whileHover={{ scale: 1.1 }}
+                                whileTap={{ scale: 0.9 }}
+                            >
+                                Update
+                            </motion.button>
+                        </form>
+                        <form onSubmit={changeDescription}>
+                            <strong>Profile Description</strong>
+                            <div>
+                                <textarea ref={description} />
+                            </div>
+                            <motion.button
+                                className="form-button"
+                                whileHover={{ scale: 1.1 }}
+                                whileTap={{ scale: 0.9 }}
+                            >
+                                Update
+                            </motion.button>
+                        </form>
+                    </div>
+                </div>
+            </motion.div>
+        </div>
+    )
+}
