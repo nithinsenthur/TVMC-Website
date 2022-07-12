b9718e06a1ed3967e52cca702c40284bf4ceb4f5
Nithin SenthurJeyamurugan <nithinkrsenthur@gmail.com>
2022-07-11 20:09:31 -0700

    Removing cached files

--- client/src/routes/auth/logout.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { useAuth } from '../../services/UsersService'
-import { Redirect } from 'react-router'
-
-export default function LogOut() {
-    const { logout } = useAuth()
-    logout()
-    return <Redirect to="/login" />
-}
